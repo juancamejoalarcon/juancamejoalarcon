20d25bc2bb97b144ffa78acd0bc7f6cab8ad4ecd
Juan Camejo Alarcón <juancamejo93@gmail.com>
2021-05-06 22:57:38 +0200

    wip

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>Mi pagina personal</div>
+      <div>Mi pagina personal es una caca</div>
     </div>
   );
 }
