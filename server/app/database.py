a3309f3bfafba24000973ef1c8592ced3530ba41
Juan Camejo Alarcón <juancamejo93@gmail.com>
2021-05-11 19:11:07 +0200

    fix url database

--- server/app/database.py
+++ server/app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL_FIXED")
 
 print(SQLALCHEMY_DATABASE_URL)
 
