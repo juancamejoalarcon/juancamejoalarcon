5de4151757e723bfb8d64a3e369b634d9f9025d1
Juan Camejo Alarcón <juancamejo93@gmail.com>
2021-05-29 20:47:05 +0200

    change social icons

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -20,22 +20,22 @@ class Footer extends React.Component<FooterProps, FooterState> {
           <div className="footer__social-links">
             <ul className="jca-social-icons medium-icons">
               <li>
-                <a href="https://www.instagram.com/"
+                <a href="https://www.instagram.com/juancamejoalarcon/"
                 ><i className="fa fa-instagram" /></a
                 >
               </li>
               <li>
-                <a href="https://twitter.com/"
+                <a href="https://twitter.com/juancamejoalarc/"
                 ><i className="fa fa-twitter" /></a
                 >
               </li>
               <li>
-                <a href="https://www.linkedin.com/"
+                <a href="https://www.linkedin.com/in/juan-camejo-alarc%C3%B3n-87692873/"
                 ><i className="fa fa-linkedin" /></a
                 >
               </li>
               <li>
-                <a href="https://github.com/"
+                <a href="https://github.com/juancamejoalarcon/"
                 ><i className="fa fa-github" /></a
                 >
               </li>
